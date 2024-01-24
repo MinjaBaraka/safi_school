25967662374cdaa7eddc3f6e0faabe184b441cb6
MinjaBaraka <45535420+MinjaBaraka@users.noreply.github.com>
2024-01-24 16:10:13 +0300

    Now
    Update SRC folrder

--- src/components/included/Nav.vue
+++ src/components/included/Nav.vue
@@ -36,7 +36,7 @@
             <img :src="Logo" alt="School Logo" class="md:w-32 w-24 mt-2">
 
             <!-- Menu -->
-            <div class="hidden md:flex space-x-8">
+            <div class="hidden xl:flex space-x-8">
                 <!-- <p v-for="option, optionIndex in menu" :key="optionIndex" @click="openLink(option.link)" class="cursor-pointer hover:text-amber-500 hover:underline hover:decoration-amber-500">
                     {{ option.title }}
                 </p> -->
@@ -47,14 +47,14 @@
             </div>
 
             <!-- Apply Now Button -->
-           <div class=" hidden md:block flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+           <div class=" hidden xl:block flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
                         <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-amber-500 hover:bg-amber-600 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                             Apply Now
                         </a>
                     </div>
 
             <!-- HeroIcon Menu -->
-            <div class="block md:hidden focus:outline-none  right-8">
+            <div class="block xl:hidden focus:outline-none  right-8">
                 <a href="" class="cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="w-12 h-12 stroke-amber-500 ">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
--- src/components/moreBlogs/DetailBlogs.vue
+++ src/components/moreBlogs/DetailBlogs.vue
@@ -89,10 +89,9 @@ const blogsNews = [
 
 
             <!-- Grids with Blogs & News -->
-            <div class="container flex flex-col md:grid lg:grid-cols-3 gap-4 grid-cols-2 md:mx-auto mt-8 mb-10">
+            <div class="xl:container flex flex-col md:grid lg:grid-cols-3 xl:gap-4 grid-cols-2 md:mx-auto mt-8 mb-10">
                   <div class=" shadow-md md:w-96 md:h-96 rounded-md cursor-pointer bg-gray-100 mb-4" v-for="object, objectIndex in blogsNews" :key="objectIndex">
 
-                    <!-- Picture BlogsNews -->
                     <div class="h-48 w-80  items-center my-4 mx-8">
                         <img :src="object.pictureEvent" alt="BlogsNews1"  class="rounded-md w-full h-full object-cover">
                     <div class="relative -top-10 left-56 bg-blue-500 h-20 w-20 justify-center rounded-full">
@@ -105,8 +104,8 @@ const blogsNews = [
             </div>
 
             <!-- Pagination -->
-        <nav aria-label="Page navigation example" >
-      <ul class="inline-flex -space-x-px text-base h-10 mb-10 px-[40%] ">
+        <nav aria-label="Page navigation example" class="">
+      <ul class="container inline-flex  -space-x-px text-base h-10 mb-10 lg:px-[40%] md:px-[30%]">
         <li>
           <a href="#" class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
         </li>
