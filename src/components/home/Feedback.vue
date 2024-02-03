315f11f10e2960aa9df4e77592044134daf0d58a
MinjaBaraka <45535420+MinjaBaraka@users.noreply.github.com>
2024-02-03 11:38:13 +0300

    03/02/2024
    swiper works

--- src/components/home/Feedback.vue
+++ src/components/home/Feedback.vue
@@ -1,8 +1,77 @@
-<script setup>
-    import FeedBack1 from '../../assets/images/feedBack/feedback1.jpg'
-    import FeedBack2 from '../../assets/images/feedBack/feedback2.jpg'
-    import FeedBack3 from '../../assets/images/feedBack/feedback3.jpg'
-    import Testimonial from '../../assets/images/feedBack/testimonial.png'
+<script >
+    import testimonial from '../../assets/images/feedBack/testimonial.png'
+
+    import { ref } from 'vue';
+
+
+    // import Swiper core and required modules
+        import { Autoplay, Pagination  } from 'swiper/modules';
+
+    // Import Swiper Vue.js components
+        import { Swiper, SwiperSlide } from 'swiper/vue';
+
+        // Import Swiper styles
+        import 'swiper/css';
+        import 'swiper/css/navigation';
+        import 'swiper/css/pagination';
+        import 'swiper/css/scrollbar';
+
+
+    export default {
+
+        components: {
+            Swiper,
+            SwiperSlide,
+        },
+
+        setup() {
+            testimonial:testimonial;
+
+            const testimonialList = ref([
+
+                {
+                    name: 'Troy Hall',
+                    whoIs: 'A customer',
+                    subtitle: 'At Safi Schools we believe that total success of our pupils depends on close and effective cooperation between the school, parent, the wider community and the various government eduction organs esponsible for regulating and advancement of education...',
+                },
+
+                {
+                    name: 'James Hall',
+                    whoIs: 'user',
+                    subtitle: 'At Safi Schools we believe that total success of our pupils depends on close and effective cooperation between the school, parent, the wider community and the various government eduction organs esponsible for regulating and advancement of education...',
+                },
+
+                {
+                    name: 'Troy Hall',
+                    whoIs: 'Parent',
+                    subtitle: 'At Safi Schools we believe that total success of our pupils depends on close and effective cooperation between the school, parent, the wider community and the various government eduction organs esponsible for regulating and advancement of education...',
+                },
+
+                {
+                    name: 'Legendary Hall',
+                    whoIs: 'Parent',
+                    subtitle: 'At Safi Schools we believe that total success of our pupils depends on close and effective cooperation between the school, parent, the wider community and the various government eduction organs esponsible for regulating and advancement of education...',
+                },
+            ])
+
+                 const onSwiper = (swiper) => {
+                    console.log(swiper);
+                };
+                const onSlideChange = () => {
+                    console.log('slide change');
+                };
+
+            return{
+                testimonial,
+                testimonialList, 
+                onSwiper,
+                onSlideChange,
+                modules: [Autoplay, Pagination],
+            }
+        }
+
+        
+    }
 </script>
 
 <template>
@@ -14,47 +83,60 @@
                     <h4 class="uppercase text-gray-500 font-serif mt-2">Testimonial</h4>
                     <h1 class="text-4xl mt-2 font-semibold">Our Parents <span class="text-green-500 font-bold">Feedback.</span></h1>
 
-                    <div class=" mt-10 ">
-                        <svg
-             xmlns="http://www.w3.org/2000/svg"
-             
-             class="inline-block h-10 w-10 pr-2 fill-current text-green-600"
-             viewBox="0 0 24 24">
-             <path
-               d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
-           </svg>
+                     <!-- <div class=" mt-10" >
+                        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-10 w-10 pr-2 fill-current text-green-600" viewBox="0 0 24 24"><path
+                            d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
+                        </svg>
+
                         <p class="text-justify text-gray-400 lg:text-left">
                             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;At Safi Schools we believe that total success of our pupils depends on close and effective
                             cooperation between the school, parent, the wider community and the various government eduction organs
                             responsible for regulating and advancement of education...
                         </p>
 
-                    </div>
+                    </div> -->
 
-                    <div class="mt-10">
-                         <div class="text-left items-center">
-                                 <h1 class="lg:text-3xl text-2xl  font-semibold uppercase">Troy Hall</h1>
+                    <swiper  class="mt-10"
+                        :modules="modules"
+                        :slides-per-view="1"
+                        :autoplay="{
+                                delay: 7000,
+                                disableOnInteraction: false,
+                            }"
+                        :thumbs="{ swiper: thumbsSwiper }"
+                        :pagination="{ clickable: true }"
+                        @swiper="onSwiper"
+                        @slideChange="onSlideChange"
+                    >
+                          <SwiperSlide v-for="object, objectIndex in testimonialList" :key="objectIndex">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-10 w-10 pr-2 fill-current text-green-600" viewBox="0 0 24 24"><path
+                                    d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
+                                </svg>
 
-                                  <h3 class="text-xs mt-1 text-gray-400">Head Of School</h3>
-                             </div>
-                    </div>
+                                    <p class="text-justify text-gray-400 lg:text-left">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{object.subtitle}}
+                                    </p>
+
+
+                             <div class="mt-20">
+                                <div class="text-left items-center">
+                                        <h1 class="lg:text-3xl text-3xl  font-semibold uppercase">{{ object.name }}</h1>
+
+                                        <h3 class="text-2xs mt-1 text-gray-400">{{ object.whoIs }}</h3>
+                                    </div>
+                            </div>
+                                
+                          </SwiperSlide>
+                          
+                    </swiper>
+
+                       
                 </div>
 
-                <!-- List Of User Feedback -->
-
-                <div class="container flex flex-row md:h-32 md:w-32 h-24 w-24 mx-2 md:mx-0 ring-4 ring-green-500 mt-10 md:space-x-10 space-x-8 items-center rounded-full cursor-pointer">
-                    <img :src="FeedBack1" alt="FeedBack1" class="ml-2 rounded-full md:h-28 md:w-28 h-20 w-20">
-
-                    
-                    <img :src="FeedBack2" alt="FeedBack2" class="rounded-full ring-2 ring-green-400 md:h-24 md:w-24 h-20 w-20 ">
-
-                    <img :src="FeedBack3" alt="FeedBack3" class="rounded-full ring-2 ring-green-400 md:h-24 md:w-24 h-20 w-20 ">
-                    
-                </div>
             </div>
 
             <!-- Picture User Feedback -->
 
-              <img :src="Testimonial" alt="Testimonial" class="relative top-6 left-2 hidden md:flex rounded-lg lg:h-full lg:w-full  md:h-/6">
+              <img :src="testimonial" alt="Testimonial" class="relative top-6 left-2 hidden md:flex rounded-lg lg:h-full lg:w-full  md:h-/6">
         </div>
 </template>
